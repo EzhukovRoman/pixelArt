e2d148d53f20e562bf51c39bd2a902711ea075bf
Roman <ezhukovroman@gmail.com>
2019-01-31 19:22:41 +0200

    First js adedd, color picking option

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 let pane = document.querySelector("#pane");
+let pickedColor = document.querySelector("#picked-color");
+let color = "";
+
+
+
+document.querySelector("#palete").addEventListener("click", (event)=>{
+    if(event.target.id !== "palete"){
+        color = event.target.id;
+        pickedColor.style.backgroundColor = color;
+    }    
+    event.stopPropagation();
+});
