4bfebc0c8d51b7500bb51d7ee02dcfab8f7af5f3
Roman <ezhukovroman@gmail.com>
2019-02-01 19:47:37 +0200

    first divs version

--- js/script.js
+++ js/script.js
@@ -2,12 +2,38 @@ let pane = document.querySelector("#pane");
 let pickedColor = document.querySelector("#picked-color");
 let color = "";
 
+document.querySelector("#palete").addEventListener("click", pickColor);
 
+document.querySelector("#erace").addEventListener("click", erace);
 
-document.querySelector("#palete").addEventListener("click", (event)=>{
+document.querySelector("#pane").addEventListener("mousedown", draw);
+
+document.querySelector("#pane").addEventListener("mouseup", stopDraw);
+
+function draw(){  
+    document.querySelectorAll(".cell").forEach(element=>{
+        element.addEventListener("mouseover", (event)=>{
+            event.target.style.backgroundColor = color;
+        });
+    });   
+}
+
+function stopDraw(){  
+    document.querySelectorAll(".cell").forEach(element=>{
+        element.removeEventListener("mouseover");
+    });   
+}
+
+function erace(){    
+    document.querySelectorAll(".cell").forEach(element => {
+        element.style.backgroundColor = "white";
+    });
+}
+
+function pickColor(){
     if(event.target.id !== "palete"){
         color = event.target.id;
         pickedColor.style.backgroundColor = color;
     }    
     event.stopPropagation();
-});
+}
