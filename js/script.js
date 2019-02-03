efdefa0d0ea63afacffbf08e31ec9ae48f6a1fb4
Roman <ezhukovroman@gmail.com>
2019-02-03 13:16:35 +0200

    Version 1.0

--- js/script.js
+++ js/script.js
@@ -10,17 +10,21 @@ document.querySelector("#pane").addEventListener("mousedown", draw);
 
 document.querySelector("#pane").addEventListener("mouseup", stopDraw);
 
+document.querySelector("#custom-color").addEventListener("input", pickCustomColor);
+
 function draw(){  
     document.querySelectorAll(".cell").forEach(element=>{
-        element.addEventListener("mouseover", (event)=>{
-            event.target.style.backgroundColor = color;
-        });
+        element.addEventListener("mouseenter", changeColor);
     });   
+}
+
+function changeColor(){
+        event.target.style.backgroundColor = color;   
 }
 
 function stopDraw(){  
     document.querySelectorAll(".cell").forEach(element=>{
-        element.removeEventListener("mouseover");
+        element.removeEventListener("mouseenter", changeColor);
     });   
 }
 
@@ -36,4 +40,9 @@ function pickColor(){
         pickedColor.style.backgroundColor = color;
     }    
     event.stopPropagation();
+}
+
+function pickCustomColor(event){
+    color = event.target.value;
+    pickedColor.style.backgroundColor = color;
 }
